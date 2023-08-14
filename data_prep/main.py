6fc141bbbe2338602c58176ef56317217aea53fa
psj098 <parksungjoon098@gmail.com>
2023-08-14 16:34:34 +0900

    Delete unnecessary file

--- data_prep/main.py
+++
@@ -1,9 +0,0 @@
-from data_preparation import get_data, get_cluster_df, get_regression_df
-
-def main():
-    clustering_df = get_cluster_df() 
-    
-    print(clustering_df.shape) 
-    
-if __name__ == "__main__":
-    main()
